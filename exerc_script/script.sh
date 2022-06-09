dc8a2c2f8b0cafeb82dad103a11a6358b37a7c4d
Simao Machado <simaomachadinho2000@gmail.com>
2022-06-09 11:30:54 +0000

    teste

--- exerc_script/script.sh
+++ exerc_script/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 clear
+<<<<<<< HEAD
 
 WELCOME_MSG_EN='Welcome'
 WELCOME_MSG_PT='Bem_Vindo'
@@ -32,3 +33,16 @@ fi
 
 
 
+=======
+WELCOME_MSG_EN='Welcome'
+WELCOME_MSG_PT='Bem_Vindo'
+CHOSE_LANG=$(echo $LANG | grep -o '^[a-zA-Z_]\{1,2\}')
+if [[ "${CHOSE_LANG}" == "pt" ]]; then
+    echo "                    $WELCOME_MSG_PT"
+    else
+    echo "                    $WELCOME_MSG_EN"
+fi    
+echo "---------------------------------------"
+echo "|                $WELCOME_MSG                   |"
+echo "---------------------------------------"
+>>>>>>> fd7c275 (teste)
